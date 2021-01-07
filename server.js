e918c226756a05768081cd967f12611c019e0c32
Aman Kumar <aman.kmr4@yahoo.com>
2021-01-07 23:14:20 +1100

    added process.env to get to connect to heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(logger("dev"));
 
@@ -15,10 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+})
+
 
 // routes
 app.use(require("./routes/api.js"));
@@ -26,3 +32,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
